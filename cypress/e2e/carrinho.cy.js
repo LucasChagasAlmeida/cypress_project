d8f7340b43be89ce1f0c27a12cfb5f0a42c27771
LucasChagasAlmeida <lucas_chagasalmeida@hotmail.com>
2025-08-20 23:52:19 -0300

    adicionando testes para o carrinho

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -1,5 +1,16 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    //arrange
+    cy.visit('https://www.saucedemo.com')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+  //act
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+
+  //assert
+    cy.get('[data-test="shopping-cart-link"]').should('be.visible').and('have.text', '1')
+
   })
 })
